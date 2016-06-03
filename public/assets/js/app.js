bdd9602e9df6a2a47ee692dfe00aa3c3eb67a685
teesloane <tylersloane@gmail.com>
2016-06-03 00:39:47 -0400

    updated File-ops. Save + save as, new file etc.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,13 +8,23 @@ init()
 
 // ui event elements.
 let els = {
+  new: document.getElementById('new-file'),
   open: document.getElementById('open-file'),
+  saveAs: document.getElementById('saveas-file'),
   save: document.getElementById('save-file')
 }
 
 // event listeners
+els.new.addEventListener('click', () => {
+  file.newFile()
+})
+
 els.open.addEventListener('click', () => {
   file.open()
+})
+
+els.saveAs.addEventListener('click', () => {
+  file.saveAs()
 })
 
 els.save.addEventListener('click', () => {
--- public/assets/js/file-io.js
+++ public/assets/js/file-io.js
@@ -6,6 +6,8 @@
 const fs = require('fs')
 const {dialog} = require('electron').remote
 
+let currentFile;
+
 const file = {
 
   open () {
@@ -23,12 +25,16 @@ const file = {
       fs.readFile(file, 'utf-8', function (err, data) {
         if (err) throw err
         document.getElementById('editor').value = data
+        // TODO: store the file path in a variable.
+        currentFile = file;
+
         // TODO: alert that file was saved
       })
     })
   },
 
-  save () {
+  saveAs () {
+    console.log('save as');
     // open the dialog box
     dialog.showSaveDialog({
       title: 'Save your document',
@@ -39,14 +45,49 @@ const file = {
     function (fileOut) {
       if (fileOut === undefined) return
       fs.writeFile(fileOut, document.getElementById('editor').value, function (err) {
+        currentFile = fileOut
         if (err) throw err
+
       })
     })
   },
 
-  new () {
-    console.log('new file created')
+  save () {
+    // if file hasn't been saved, run saveAs()
+    if (!currentFile) {
+      this.saveAs();
+    } else {
+      fs.writeFile(currentFile.toString(), document.getElementById('editor').value, function (err) {
+        if (err) throw err
+      })
+    }
+  },
+
+  newFile () {
+    let editor = document.getElementById('editor')
+
+    // if there is text in the editor
+    if (editor.value !== '') {
+
+      // TODO: if check on editor.value against actual file to see if things have changed / the file should be saved first.
+      // I wonder if a diff on a large file would be costly...
+
+      dialog.showMessageBox({
+        type: 'warning',
+        buttons: ['Cancel', 'New File'],
+        title: 'Unsaved Text',
+        message: 'You still have some unsaved work kickin\' around. You sure you want to make a new file?'
+      }, function(rdata) {
+          if (rdata === 1) {
+            document.getElementById('editor').value = '';
+            currentFile = null;
+          }
+      })
+    } else {
+      return
+    }
   }
+
 }
 
 module.exports = file
