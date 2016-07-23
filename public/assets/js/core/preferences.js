f6b1dc3d390f49724d3b045f0cdafeda3cb94bfe
teesloane <tylersloane@gmail.com>
2016-07-22 21:07:56 -0400

    Basic pref window.

--- public/assets/js/core/preferences.js
+++ public/assets/js/core/preferences.js
@@ -1,7 +1,6 @@
 const BrowserWindow = require('electron').remote.BrowserWindow
 const path = require('path')
 const template = path.join(__dirname, '../../../preferences.html')
-console.log(template) 
 
 let prefWindow = null
 
@@ -9,9 +8,23 @@ const preferences = {
   // open the preferences window
   open () {
     if (prefWindow !== null) return
-    prefWindow = new BrowserWindow({width: 300, height: 400})
+
+    prefWindow = new BrowserWindow({
+      width: 300,
+      height: 300,
+      show: false,
+      resizable: false
+    })
+
+    prefWindow.once('ready-to-show', () => {
+      prefWindow.show()
+    })
+
     prefWindow.on('closed', () => { prefWindow = null })
-  prefWindow.loadURL(`file://${template}`)
+
+    prefWindow.setMenu(null)
+    
+    prefWindow.loadURL(`file://${template}`)
   }
 }
 
