02f835057a6d907c8ffdab49a9bf4e3166afee53
teesloane <tylersloane@gmail.com>
2016-07-30 22:52:17 -0400

    File naming, styling markdown etc

--- public/assets/js/setup.js
+++ public/assets/js/setup.js
@@ -2,14 +2,14 @@ const createAppMenu = require('./core/menu-app')
 const createAmbientMenu = require('./core/menu-ambient')
 const createMixer = require('./core/menu-mixer')
 const settings = require('./core/settings')
-const createUpdateEventListener = require('./core/content-update')
+const markdownUpdate = require('./core/content-update')
 
 const setup = function () {
   settings.setDefault()
   createAppMenu()
   createAmbientMenu()
   createMixer()
-  createUpdateEventListener()
+  markdownUpdate()
 }
 
 setup()
